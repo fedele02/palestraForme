bc0094ab46aedeb60b96d96a57aa768347c441b0
Fedele De Biasi <Debiasifedele31@gmail.com>
2026-04-21 19:51:34 +0200

    sistemazione loghi

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,36 +1,7 @@
 export const Logo = ({ className = "" }) => (
-  <svg 
-    viewBox="0 0 200 200" 
-    fill="none" 
-    xmlns="http://www.w3.org/2000/svg"
-    className={className}
-  >
-    {/* Linea Gialla del 4 */}
-     <path 
-      d="M 125 90
-         L 125 35 
-         C 125 25, 115 20, 108 30 
-         L 45 135 
-         C 40 143, 45 150, 55 150 
-         L 75 150 
-         L 79 125 
-         L 85 175 
-         L 90 135 
-         L 93 150 
-         L 105 150 
-         L 105 175 
-         C 105 185, 125 185, 125 175 
-         L 125 145 
-         L 132 145"
-      stroke="#F7E842" 
-      strokeWidth="5" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    />
-    
-    {/* Testo ME e sottotitoli */}
-    <text x="96" y="115" fill="white" fontSize="48" fontWeight="900" fontFamily="Montserrat" letterSpacing="-1">ME</text>
-    <text x="96" y="128" fill="white" fontSize="9" fontWeight="700" fontFamily="Montserrat" letterSpacing="0.5">POWER FITNESS</text>
-    <text x="96" y="139" fill="white" fontSize="9" fontWeight="700" fontFamily="Montserrat" letterSpacing="0.5">EXPERIENCE</text>
-  </svg>
+  <img
+    src={`${import.meta.env.BASE_URL}icona2.png?v=2`}
+    alt="Logo ME Power Fitness Experience"
+    className={`${className} object-contain`}
+  />
 );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const faviconHref = `${import.meta.env.BASE_URL}favicon-logo2.svg?v=5`;
+let favicon = document.querySelector("link[rel='icon']");
+if (!favicon) {
+  favicon = document.createElement('link');
+  favicon.setAttribute('rel', 'icon');
+  document.head.appendChild(favicon);
+}
+favicon.setAttribute('type', 'image/svg+xml');
+favicon.setAttribute('href', faviconHref);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
